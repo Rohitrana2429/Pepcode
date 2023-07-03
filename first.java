cdfca631a05c03b9d5bcfc62c2b7365706fc7d29
ROHIT RANA <83514950+Rohitrana2429@users.noreply.github.com>
2023-07-03 22:27:38 +0530

    Create first.java

---
+++ first.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class first {
+    public static void main(String[] args) {
+      int n =40;
+      if(n>90)
+      {
+        System.out.println("Excellent");
+      }
+      else if(n>80 && n<90)
+      {
+        System.out.println("good");
+      }
+      else if(n>70 && n<80)
+      {
+       System.out.println("fair");
+      }
+      else if(n>60 &&n<70)
+      {
+        System.out.println("Expectation");
+      }
+      else{
+        System.out.println("below pair");
+      }
+  }
+}
